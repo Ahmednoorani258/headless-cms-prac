c9b0a46db08d06aa65e4d053a0a5ecbcc0475bc9
muhammad ahmed noorani <ahmednoorani258@gmail.com>
2024-12-12 23:18:47 -0800

    schemas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
+import { client } from "@/sanity/lib/client";
 
-export default function Home() {
+async function getData(){
+  const fetchData = await client.fetch(`*[_type == 'student']`)
+  return fetchData
+}
+
+export default async function Home() {
+  
+  const data = await getData()
+  console.log(data)
   return (
-    <div>hi</div>
+    <div className="flex justify-center items-center">
+      {data.map((val:any, id:number) => 
+          
+          <h1 key={id}>{val.name}</h1>
+          
+      )}
+      hh
+    </div>
   );
 }
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,9 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { eventType } from "@/sanity/schemaTypes/eventTypes";
+import { artistType } from "./artistType";
+import { venueType } from "./venueType";
+import { student } from "./student";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [eventType, artistType, venueType, student],
+};
